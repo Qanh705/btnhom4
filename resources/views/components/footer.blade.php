382ec52e967da88eb3c077ee5ad55231569f432b
Qanh705 <030238220007@st.buh.edu.vn>
2025-04-11 17:54:26 +0700

    file home cho trang chủ + file about cho liên hệ (trong view) T sài Homecontroller cho cả 2 trang này nhé

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,33 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Email</h3>
+         <a href="mailto:smarttech@gmail.com">smarttech@gmail.com</a>
+         <a href="mailto:quamonltw@gmail.com">quamonltw@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <h3>Giờ mở cửa</h3>
+         <p>07:00am to 22:00pm</p>
+      </div>
+
+      <div class="box">
+         <h3>Địa chỉ</h3>
+         <a href="#">Trường đại học Ngân Hàng TP.HCM</a>
+      </div>
+
+      <div class="box">
+         <h3>Hotline</h3>
+         <a href="tel:0000000000">000-000-0000</a>
+         <a href="tel:0123456789">012-345-6789</a>
+      </div>
+
+   </section>
+
+   <div class="credit">
+      &copy; Thành lập: {{ date('Y') }} by <span>Nhom5lopthayKien</span> | Sự lựa chọn tuyệt vời
+   </div>
+
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', 'App\Http\Controllers\HomeController@index');
+Route::get('/about', 'App\Http\Controllers\HomeController@about');
